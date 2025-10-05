af286abba4377d56d5307ad14ce3ba0707c2defa
DavelDs <163164280+DavelDs@users.noreply.github.com>
2025-10-05 19:56:41 +0700

    Commit No.2 - Clickable RecyclerView

--- app/src/main/java/com/example/lab_week_06/MainActivity.kt
+++ app/src/main/java/com/example/lab_week_06/MainActivity.kt
@@ -7,6 +7,8 @@ import androidx.recyclerview.widget.RecyclerView
 import com.example.lab_week_06.model.CatModel
 import com.example.lab_week_06.model.CatBreed
 import com.example.lab_week_06.model.Gender
+import androidx.appcompat.app.AlertDialog
+
 
 
 class MainActivity : AppCompatActivity() {
@@ -15,7 +17,12 @@ class MainActivity : AppCompatActivity() {
     }
     private val catAdapter by lazy {
 //Glide is used here to load the images
-        CatAdapter(layoutInflater, GlideImageLoader(this))
+        //Here we are passing the onClickListener function to the Adapter
+        CatAdapter(layoutInflater, GlideImageLoader(this), object:
+            CatAdapter.OnClickListener {
+            //When this is triggered, the pop up dialog will be shown
+            override fun onItemClick(cat: CatModel) = showSelectionDialog(cat)
+        })
     }
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
@@ -54,4 +61,15 @@ class MainActivity : AppCompatActivity() {
             )
         )
     }
+    //This will create a pop up dialog when one of the items from the recycler view
+    private fun showSelectionDialog(cat: CatModel) {
+        AlertDialog.Builder(this)
+//Set the title for the dialog
+            .setTitle("Cat Selected")
+//Set the message for the dialog
+            .setMessage("You have selected cat ${cat.name}")
+//Set if the OK button should be enabled
+            .setPositiveButton("OK") { _, _ -> }.show()
+    }
+
 }
